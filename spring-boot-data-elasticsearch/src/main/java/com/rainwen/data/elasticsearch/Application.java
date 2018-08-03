de897439bc2d944b469f992e366912e9d23a466a
rain.wen <nassir@126.com>
2018-08-04 01:30:52 +0800

    增加测试数据和地图显示demo

--- spring-boot-data-elasticsearch/src/main/java/com/rainwen/data/elasticsearch/Application.java
+++ spring-boot-data-elasticsearch/src/main/java/com/rainwen/data/elasticsearch/Application.java
@@ -3,6 +3,8 @@ package com.rainwen.data.elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author rain.wen
  * @since 2018/7/23 19:17
@@ -10,7 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class);
+        new CountDownLatch(1).await();
     }
 }
---
+++ spring-boot-data-elasticsearch/src/main/java/com/rainwen/data/elasticsearch/controller/SearchController.java
@@ -0,0 +1,53 @@
+package com.rainwen.data.elasticsearch.controller;
+
+import com.rainwen.data.elasticsearch.model.WeiboCheckin;
+import com.rainwen.data.elasticsearch.service.WeiboCheckinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ *
+ */
+@RestController
+public class SearchController {
+
+    @Autowired
+    private WeiboCheckinService weiboCheckinService;
+
+
+    /**
+     * 按地址查询签到次数降序前N条
+     *
+     * @return
+     */
+    @RequestMapping("/search")
+    public List<WeiboCheckin> search(@RequestParam(name = "placeName", required = false) String placeName,
+                                    @RequestParam(name = "topN", defaultValue = "10") Integer topN) {
+        List<WeiboCheckin> weiboCheckinList = weiboCheckinService.findByPlaceNameOrderByCheckinNumDesc(placeName, topN);
+        return weiboCheckinList;
+    }
+
+    /**
+     * 按距离最近查询前N条
+     *
+     * @return
+     */
+    @RequestMapping("/searchNearby")
+    public List<WeiboCheckin> searchNearby(@RequestParam(name = "lat") Double lat,
+                                           @RequestParam(name = "lon") Double lon,
+                                           @RequestParam(name = "topN", defaultValue = "10") Integer topN) {
+        Pageable page = PageRequest.of(0, topN);
+
+        Page<WeiboCheckin> weiboCheckinPage = weiboCheckinService.findNearbyByDistance(lat, lon, page);
+
+        return weiboCheckinPage.getContent();
+    }
+
+}
